8316ce1c1f8446f600817c754433d5ab24e93754
Francesco Alemanno <francescoalemanno710@gmail.com>
2020-08-16 18:14:01 +0200

    optimization interface

--- src/ExtremalOptimization.jl
+++ src/ExtremalOptimization.jl
@@ -1,24 +1,61 @@
 module ExtremalOptimization
     using Random, StatsBase
 
-    function optimize(f,s,N;γ=1.2,β=1.0,rng=Random.GLOBAL_RNG,reps_per_particle=100)
-        P = [s(i) for i=1:N]
-        C = [f(P[i]) for i=1:N]
+    struct EOState{vP,vC,vO,tW,tN}
+        P::Vector{vP}
+        C::Vector{vC}
+        order::Vector{vO}
+        W::tW
+        N::tN
+    end
+
+    struct EOProblem{tF,tS}
+        f::tF
+        s::tS
+    end
+
+    function eostate(problem::EOProblem,N; γ, kw...)
+        P = [problem.s(i) for i=1:N]
+        C = [problem.f(P[i]) for i=1:N]
         order = sortperm(C)
         W = Weights([i^(-γ) for i=N:-1:1])
-        for reps = 1:(reps_per_particle*N)
-            i = order[sample(rng,W)]
-            j = i
-            while j==i; j=rand(rng,1:N); end
-            α=(1 + β * abs2(randn(rng)))/2
-            nP=P[i]+α*(P[j]-P[i])
-            nC=f(nP)
-            P[i]=nP
-            C[i]=nC
-            sortperm!(order,C)
-        end
-        best=order[begin]
-        (x=P[best],cost=C[best],pop=P[order],costs=C[order])
+        best=order[1]
+        P[best], C[best],  EOState(P,C,order,W,N)
     end
+
+    function sample_distinct(rng,N,set,notset)
+        N <= 0 && return ()
+        @label resample
+        s = rand(rng,set)
+        (s ∈ notset) && @goto resample
+        return (s, sample_distinct(rng,N-1,set,(notset...,s))...)
+    end    
+
+    function eostate(problem::EOProblem,S::EOState; β, rng, kw...)
+        α = β*randn(rng)
+        i = S.order[sample(rng,S.W)]
+        j, k, q = sample_distinct(rng,3,1:S.N,(i,))
+        nP = S.P[j] + α * (S.P[k] - S.P[q])
+        nC = problem.f(nP)
+        S.P[i] = nP
+        S.C[i] = nC
+        sortperm!(S.order,S.C)
+        best = S.order[1]
+        S.P[best], S.C[best], EOState(S.P,S.C,S.order,S.W,S.N)
+    end
+
+    function optimize(f,s,N;β=1.0,γ=1.2,rng=Random.GLOBAL_RNG, reps_per_particle=100, callback=state->nothing)
+        problem=EOProblem(f,s)
+        best, cost, state=eostate(problem,N;γ)
+        callback(state)
+        for i in 1:(N*reps_per_particle)
+            best, cost, state=eostate(problem,state; β,rng)
+            callback(state)
+        end
+        (x=best, fx=cost)
+    end
+
     export optimize
 end
+
+
--- test/runtests.jl
+++ test/runtests.jl
@@ -5,8 +5,5 @@ using Test, Random
     sol = optimize(x->(x-1)^2,i->randn(),50)
     @test sol.x ≈ 1.0
     sol = optimize(x->(x[1]-1)^2+(x[2]-x[1]^2)^2,i->randn(2),50,reps_per_particle=200)
-    @test all(sol.x .≈ 1)   
+    @test all(sol.x .≈ 1)
 end
-
-
-sol = optimize(x->(x[1]-1)^2+(x[2]-x[1]^2)^2,i->randn(2),50,reps_per_particle=200,β=sqrt(3))
